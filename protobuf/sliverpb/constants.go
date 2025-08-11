fbd8d8c6a86dbfb7dc8b9df4e891fb7511e6cefb
papcaii <duyhn2004.work@gmail.com>
2025-08-11 09:02:58 +0700

    refactor protobuf

--- protobuf/sliverpb/constants.go
+++ protobuf/sliverpb/constants.go
@@ -459,7 +459,7 @@ func MsgNumber(request proto.Message) uint32 {
 		return MsgTerminateReq
 	case *Terminate:
 		return MsgTerminate
-	case *ScreenshotReq:
+	case *ScShotReq:
 		return MsgScreenshotReq
 	case *Screenshot:
 		return MsgScreenshot
